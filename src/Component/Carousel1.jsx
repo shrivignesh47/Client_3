e624b1a92fb8cea4df1cdacc4bf6936f93754e77
Shrivignesh47 <shrivigneshshrivignesh4@gmail.com>
2025-02-22 01:47:24 +0530

    removed_timer

--- src/Component/Carousel1.jsx
+++ src/Component/Carousel1.jsx
@@ -1,6 +1,5 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-// import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import '../Styles/Swiper.css';
@@ -17,14 +16,7 @@ import msoup from '../Assets/mobile_responsive/msoup.png';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 export default function Car() {
-  const progressCircle = useRef(null);
-  const progressContent = useRef(null);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-
-  const onAutoplayTimeLeft = (s, time, progress) => {
-    progressCircle.current.style.setProperty('--progress', 1 - progress);
-    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
-  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -40,36 +32,27 @@ export default function Car() {
     : [img1, img4, img3, img6, img7];
 
   return (
-    <>
-      <Swiper
-        spaceBetween={0}
-        centeredSlides={true}
-        loop={true}
-        speed={1000}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: true,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        onAutoplayTimeLeft={onAutoplayTimeLeft}
-        className="mySwiper mt-0 md:m-[2vh]" // Add margin-top for mobile, remove for larger screens
-      >
-        {images.map((img, index) => (
-          <SwiperSlide key={index}>
-            <img src={img} alt={`Delicious Dish ${index + 1}`} />
-          </SwiperSlide>
-        ))}
-        <div className="autoplay-progress" slot="container-end">
-          <svg viewBox="0 0 48 48" ref={progressCircle}>
-            <circle cx="24" cy="24" r="20"></circle>
-          </svg>
-          <span ref={progressContent}></span>
-        </div>
-      </Swiper>
-    </>
+    <Swiper
+      spaceBetween={0}
+      centeredSlides={true}
+      loop={true}
+      speed={1000}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: true,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      className="mySwiper mt-0 md:m-[2vh]" // Add margin-top for mobile, remove for larger screens
+    >
+      {images.map((img, index) => (
+        <SwiperSlide key={index}>
+          <img src={img} alt={`Delicious Dish ${index + 1}`} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
